e5a93381ce203755af0013b219a108ef02cd5754
maha507 <132257569+maha507@users.noreply.github.com>
2023-05-16 16:10:03 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,25 @@
 <script>
-	import Grid from "gridjs-svelte";
-	
-	
-		const data = [
-	  { name: "Sachin", runs: 320000, age: 40, centuries: 100,  wickets: 168 },
-	  { name: "Kohli", runs: 25000, age: 34, centuries: 75, wickets: 78 },
-	  { name: "Root", runs: 19000, age: 33, centuries: 45, wickets: 98 },
-	  { name: "Smith", runs: 17000, age: 32, centuries: 41, wickets: 119 },
-	  { name: "Pointing", runs: 22000, age: 37, centuries: 65, wickets: 68 },
-	  { name: "Kane", runs: 18000, age: 37, centuries: 35, wickets: 61 },
-	  { name: "Anderson", runs: 2000, age: 39, centuries: 1, wickets: 678 },
-	  { name: "Warne", runs: 1800, age:39, centuries: 2, wickets: 778 },
-	];
-  
-	  
+  import { onMount } from "svelte";
+  import Grid from "gridjs-svelte"
+  const endpoint = "https://jsonplaceholder.typicode.com/posts";
+  let posts = [];
 
-	const columns = ["name", "runs" , "age" , "centuries" , "Wickets"];
-	
+  onMount(async function () {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+  });
   
+  export let name;
+</script>
 
-  </script>
-  <Grid data={data} 
-   sort 
-   search 
-   pagination={{ enabled: true, limit:5 }}  />
+<main>
+  <h1>click the data</h1>
+  <p>Visit the <a href="https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3">data</a> for more details.</p>
 
-  <style global>
-  @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
-  </style>
-  
+  {#each posts as article}
+    <div>
+      <p>{article.title}</p>
+    </div>
+  {/each}
+</main>
