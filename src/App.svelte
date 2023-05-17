e3fa74371f498ef832def3c947bcdf55c34ed218
maha507 <132257569+maha507@users.noreply.github.com>
2023-05-17 14:49:09 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,19 @@
   import Grid from "gridjs-svelte";
 
   let jsonData = [];
-
+  let showTable=false;
   onMount(async () => {
     const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
     const responseData = await response.json();
     jsonData = responseData.data;
   });
+  function openTable(){
+    showTable = !showTable
+  }
 </script>
-
+<button on:click={openTable}>Display Table</button>
 <main>
+  {#if showTable}
   <Grid
   search 
   sort
@@ -25,7 +29,8 @@
       status: item.status
     }
   })} />
-    </main>
+   {/if}
+  </main>
   
 <style global>
   @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
