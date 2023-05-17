290f87d36365450cb900fd8937c6eb6cc9e26c99
maha507 <132257569+maha507@users.noreply.github.com>
2023-05-17 18:08:40 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,39 +1,46 @@
 <script>
   import { onMount } from "svelte";
-  
   import Grid from "gridjs-svelte";
 
   let jsonData = [];
-  let showTable=false;
+  let tableVisible = false;
+
   onMount(async () => {
     const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
     const responseData = await response.json();
     jsonData = responseData.data;
   });
-  function openTable(){
-    showTable = !showTable
+
+  function openTable() {
+    if (!tableVisible) {
+      fetchData();
+    }
+  }
+
+  async function fetchData() {
+    const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
+    const responseData = await response.json();
+    jsonData = responseData.data;
+    tableVisible = true;
   }
 </script>
+
 <button on:click={openTable}>Display Table</button>
+
 <main>
-  {#if showTable}
-  <Grid
-  search 
-  sort
-  pagination={{ enabled: true, limit: 10 }}
-  data={jsonData.map(item => {
-    
-    return {
-      title: item.title,
-      reference: item.reference,
-      status: item.status
-    }
-  })} />
-   {/if}
-  </main>
-  
+  {#if tableVisible}
+    <Grid
+      search
+      sort
+      pagination={{ enabled: true, limit: 10 }}
+      data={jsonData.map(item => ({
+        title: item.title,
+        reference: item.reference,
+        status: item.status
+      }))} />
+  {/if}
+</main>
+
 <style global>
   @import "https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css";
 </style>
-
-
