96026ea2b59b33ab9ae4aa45e4d60831d93e666c
maha507 <132257569+maha507@users.noreply.github.com>
2023-05-17 10:59:08 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,65 +1,66 @@
 <script>
+  import { onMount } from "svelte";
+  
+  import Grid from "gridjs-svelte";
 
-      import { onMount } from "svelte";
-      let jsonData = [];
-  
-     onMount(async () => {
-  
-        const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
-        const responseData = await response.json();
-        jsonData = responseData.data;
-  
-      });
-  
-    </script>
-     <main>
-  
-    {#if jsonData.length > 0}
-  
-       <div class="grid-container">
-  
-         {#each jsonData as item}
-  
-          {#each Object.entries(item) as [key, value]}
-  
-            <div class="grid-item">
-  
-               <span>{key}: </span>
-  
-             <span>{typeof value === 'object' ? JSON.stringify(value) : value}</span>
-  
-           </div>
-  
-         {/each}
-  
-       {/each}
-  
-     </div>
-  
-   {/if}
-  
-   </main>
-  
-   <style>
-  
-   main {
-   display: flex;
-   flex-direction: column;
-   align-items: center;
-   padding: 30px;
-   }
-  
-   .grid-container {
-   display: grid;
-   grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-   gap: 10px;
-   width: 100%;
+  let jsonData = [];
+
+  onMount(async () => {
+    const response = await fetch("https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3");
+    const responseData = await response.json();
+    jsonData = responseData.data;
+  });
+
+ 
+</script>
+
+<main>
+  {#if jsonData.length > 0}
+    <div class="grid-container">
+      {#each jsonData as item}
+        <div class="grid-item">
+          <span class="heading">title</span>
+          <span>{item.title}</span>
+        </div>
+        <div class="grid-item">
+          <span class="heading">reference</span>
+          <span>{item.reference}</span>
+        </div>
+        <div class="grid-item">
+          <span class="heading">status</span>
+          <span>{item.status}</span>
+        </div>
+      {/each}
+    </div>
+   
+  {/if}
+</main>
+
+
+<style>
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
   }
-  
+
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr); 
+    gap: 10px;
+    width: 50%;
+  }
+
   .grid-item {
-   border: 2px solid #ddd;
-   padding: 10px;
-   text-align: left;
-   }
-  
+    border: 1px solid #ddd;
+    padding: 5px;
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .heading {
+    font-weight: bold;
+  }
 </style>
